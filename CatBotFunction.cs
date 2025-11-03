da5459f804bab254da5721c04025f594845fdfe5
dramlian <jankovdamian@gmail.com>
2025-11-03 21:46:59 +0100

    Made it into a Http function

--- CatBotFunction.cs
+++ CatBotFunction.cs
@@ -1,5 +1,7 @@
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Net;
 
 namespace CATBOT.Function;
 
@@ -13,24 +15,33 @@ public class CatBotFunction
     }
 
     [Function("CatBotFunction")]
-    public async Task Run([TimerTrigger("*/10 * * * * *")] TimerInfo myTimer)
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
     {
+        var response = req.CreateResponse(HttpStatusCode.OK);
+
         var PAT = Environment.GetEnvironmentVariable("META_PAT");
         var recipientID = Environment.GetEnvironmentVariable("RECIPIENT_ID");
 
         if (string.IsNullOrEmpty(PAT))
         {
             _logger.LogError("META_PAT environment variable is not set");
-            return;
+            response.StatusCode = HttpStatusCode.InternalServerError;
+            await response.WriteStringAsync("META_PAT environment variable is not set");
+            return response;
         }
 
         if (string.IsNullOrEmpty(recipientID))
         {
             _logger.LogError("RECIPIENT_ID environment variable is not set");
-            return;
+            response.StatusCode = HttpStatusCode.InternalServerError;
+            await response.WriteStringAsync("RECIPIENT_ID environment variable is not set");
+            return response;
         }
 
         ICatImageSender catImageSender = new CatImageSender();
         await catImageSender.SendCatImage(PAT, recipientID);
+
+        await response.WriteStringAsync("Cat image sent successfully!");
+        return response;
     }
 }
