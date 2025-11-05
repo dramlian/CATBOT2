d8d696cde3099f7da21df2e8d578e0e3ca4d92b4
dramlian <jankovdamian@gmail.com>
2025-11-05 20:58:41 +0100

    Working azure function, that also works in deployment! With Get request for verification

--- CatBotFunction.cs
+++ CatBotFunction.cs
@@ -2,6 +2,8 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using System.Net;
+using System.Text;
+using System.Text.Json;
 
 namespace CATBOT.Function;
 
@@ -15,33 +17,59 @@ public class CatBotFunction
     }
 
     [Function("CatBotFunction")]
-    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
     {
-        var response = req.CreateResponse(HttpStatusCode.OK);
+        var query = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
 
-        var PAT = Environment.GetEnvironmentVariable("META_PAT");
-        var recipientID = Environment.GetEnvironmentVariable("RECIPIENT_ID");
-
-        if (string.IsNullOrEmpty(PAT))
+        if (req.Method == "GET")
         {
-            _logger.LogError("META_PAT environment variable is not set");
-            response.StatusCode = HttpStatusCode.InternalServerError;
-            await response.WriteStringAsync("META_PAT environment variable is not set");
+            var mode = query["hub.mode"];
+            var token = query["hub.verify_token"];
+            var challenge = query["hub.challenge"];
+
+            if (mode == "subscribe" && token == Environment.GetEnvironmentVariable("VERIFY_TOKEN") && !String.IsNullOrEmpty(challenge))
+            {
+                var response = req.CreateResponse(HttpStatusCode.OK);
+                await response.WriteStringAsync(challenge, Encoding.UTF8);
+                return response;
+            }
+
+            return req.CreateResponse(HttpStatusCode.Forbidden);
+        }
+        else if (req.Method == "POST")
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            _logger.LogInformation($"Received POST payload: {requestBody}");
+
+            var senderId = JsonDocument.Parse(requestBody).RootElement
+                .GetProperty("entry")[0]
+                .GetProperty("messaging")[0]
+                .GetProperty("sender")
+                .GetProperty("id")
+                .GetString() ?? throw new Exception("No sender ID was found!");
+
+            _logger.LogInformation($"Sender ID: {senderId}");
+            var PAT = Environment.GetEnvironmentVariable("META_PAT");
+
+            if (string.IsNullOrEmpty(PAT))
+            {
+                _logger.LogError("META_PAT environment variable is not set");
+                var error = req.CreateResponse(HttpStatusCode.InternalServerError);
+                await error.WriteStringAsync("META_PAT environment variable is not set");
+                return error;
+            }
+
+            ICatImageSender catImageSender = new CatImageSender();
+            await catImageSender.SendCatImage(PAT, senderId);
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            await response.WriteStringAsync("Cat image sent successfully!");
             return response;
         }
-
-        if (string.IsNullOrEmpty(recipientID))
+        else
         {
-            _logger.LogError("RECIPIENT_ID environment variable is not set");
-            response.StatusCode = HttpStatusCode.InternalServerError;
-            await response.WriteStringAsync("RECIPIENT_ID environment variable is not set");
-            return response;
+            var responsee = req.CreateResponse(HttpStatusCode.NotFound);
+            return responsee;
         }
-
-        ICatImageSender catImageSender = new CatImageSender();
-        await catImageSender.SendCatImage(PAT, recipientID);
-
-        await response.WriteStringAsync("Cat image sent successfully!");
-        return response;
     }
 }
